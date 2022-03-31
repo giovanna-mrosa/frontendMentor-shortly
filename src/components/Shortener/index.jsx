5537682b113764381dab45c2781d9c5e53b7d974
Giovanna Rosa <gmarcaro@gmail.com>
2022-03-31 12:21:49 -0300

    input validation

--- src/components/Shortener/index.jsx
+++ src/components/Shortener/index.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import validator from 'validator';
 
 import './styles.scss'
 
 export function Shortener() {
+  const [linkError, setLinkError] = useState('')
+  const [link, setLink] = useState('')
+  const [empty, setEmpty] = useState(false)
+
+
+
+  const validateLink = () => {
+    if (validator.isEmpty(link)) {
+      console.log("validator", link)
+      setLinkError('Please add a link') 
+      setEmpty(true)     
+    } else {
+      setLinkError('')
+      setEmpty(false)
+    }
+  }
+
+  console.log("validator", link)
+
   return (
     <div className="shortener-box">
       <div className="form-box">
@@ -11,10 +31,18 @@ export function Shortener() {
             type="text" 
             name="input-shortener" 
             id="inpt-short" 
-            className="inpt-short" 
+            className={empty === true ? "inpt-short inpt-error" : "inpt-short"} 
             placeholder="Shorten a link here..."
+            onChange={(e) => setLink(e.target.value)}
           />
-          <button type="button" className="btn-short">Shorten It!</button>
+          <span className="error-field">{linkError}</span>
+          <button 
+            type="button" 
+            className="btn-short"
+            onClick={validateLink}
+          >
+            Shorten It!
+          </button>
         </form>
       </div>
     </div>
